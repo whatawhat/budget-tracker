7c389d55d2611b19ddeed92aab48f18c9b13242d
Stephanie <whatawhat@hotmail.com>
2021-08-15 17:07:13 -0400

    updated routes to files

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/manifest.webmanifest",
-    "/styles.css",
-    "/db.js",
-    "/index.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+    "/assets/css/styles.css",
+    "/assets/js/db.js",
+    "/assets/js/index.js",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
   ];
   
   const CACHE_NAME = "static-cache-v2";
