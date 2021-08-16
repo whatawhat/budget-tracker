ef575f1cba1d8e07bfddec9bd05c38c51da20036
Stephanie <whatawhat@hotmail.com>
2021-08-16 15:07:09 -0400

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTrackerDB", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
